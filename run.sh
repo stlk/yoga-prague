63de10b499289b3c055383a8a53546d211ecc0da
Josef Rousek <josef@rousek.name>
2023-05-05 08:45:30 +0200

    Introduce actions

--- run.sh
+++ run.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Check if Pipenv is installed
-command -v pipenv >/dev/null 2>&1 || { echo >&2 "Pipenv is required, but not installed. Aborting."; exit 1; }
-
 # Run the Python scripts using Pipenv
 echo "Running scrape_dum_jogy.py"
-pipenv run python scrape_dum_jogy.py
+python scrape_dum_jogy.py
 
 echo "Running scrape_yk.py"
-pipenv run python scrape_yk.py
+python scrape_yk.py
 
 echo "Running combine_and_upload.py"
-pipenv run python combine_and_upload.py
+python combine_and_upload.py
 
 echo "All scripts executed successfully."
